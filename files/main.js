9265cd48a194d336c2c7fd454a9cbc67226a149f
huangjizhou <hjizhou@gmail.com>
2024-08-26 09:27:06 +0800

    update info

--- files/main.js
+++ files/main.js
@@ -1,7 +1,7 @@
 var item = document.querySelectorAll('.s-year');
 
 const DATA = `
-1|Chatbot2007|Natural Language Processing,Human-AI Interaction
+1|Chatbot2007|Natural Language Processing
 2|RecEvidence2016|Search and Recommendation
 3|EntityRelation2017|Search and Recommendation
 4|LTR4EntityRec2018|Search and Recommendation
@@ -9,11 +9,11 @@ const DATA = `
 6|MT4EntityRec2018|Search and Recommendation
 7|RWM2019|Computer Vision
 8|PhDThesis2019|Search and Recommendation
-9|EntityRecSurvey2019|Search and Recommendation
+9|EntityRecSurvey2019|Search and Recommendation,Survey
 10|MONOPOLY2019|Data Mining
-11|CurbsideParking2019|Intelligent Transportation
-12|EconomicCOVID2020|Data Mining
-13|ResponsesCOVID2020|Data Mining
+11|CurbsideParking2019|Data Mining
+12|ResponsesCOVID2020|Data Mining
+13|EconomicCOVID2020|Data Mining
 14|PAC2020|Search and Recommendation
 15|MultiTask4ER-DR2020|Search and Recommendation
 16|ConSTGAT2020|Intelligent Transportation
@@ -23,7 +23,7 @@ const DATA = `
 20|MeasuresCOVID2020|Data Mining
 21|CWatcher2021|Data Mining
 22|ActivitiesCOVID2021|Data Mining
-23|DialogGen2021|Natural Language Processing,Human-AI Interaction
+23|DialogGen2021|Natural Language Processing
 24|NextPOIRec2021|Search and Recommendation
 25|MLPAC2021|Search and Recommendation
 26|SSML2021|Intelligent Transportation
@@ -31,43 +31,45 @@ const DATA = `
 28|PublicResponsesCOVID2021|Data Mining
 29|DOLG2021|Computer Vision
 30|GEDIT2021|Natural Language Processing
-31|FederatedLearningsurvey2022|Artificial Intelligence
+31|FederatedLearningsurvey2022|Survey
 32|DuARE2022|Intelligent Transportation
-33|DuIVA2022|Natural Language Processing,Human-AI Interaction
-34|ERNIE-GeoL2022|Natural Language Processing
+33|DuIVA2022|Natural Language Processing
+34|ERNIE-GeoL2022|Natural Language Processing,Large Language Models (LLMs) and Applications
 35|DuTraffic2022|Intelligent Transportation
 36|DuARUS2022|Intelligent Transportation
 37|DuMapper2022|Intelligent Transportation
 38|DuETA2022|Intelligent Transportation
-39|DuIVRS2022|Natural Language Processing,Human-AI Interaction
+39|DuIVRS2022|Natural Language Processing
 40|AirPollution2023|Data Mining
 41|UrbanRegionGraph2023|Data Mining
 42|3DPointCloudGen2023|Computer Vision
 43|TSC2023|Search and Recommendation
-44|POInBlog2023|Search and Recommendation
+44|POInBlog2023|Data Mining
 45|SHAGNN2023|Data Mining
+46|UASeg2024|Data Mining
+47|E2ECausality|Autonomous Driving
+48|BEVWorld|Autonomous Driving
+49|DuMapNet2024|Intelligent Transportation
+50|ReFound2024|Data Mining
 `;
 
 // 决定tag标签整体排序
 const tagsOrder = [
-  'Human-AI Interaction',
+  'Large Language Models (LLMs) and Applications',
+  'Autonomous Driving',
   'Intelligent Transportation',
   'Data Mining',
   'Natural Language Processing',
   'Search and Recommendation',
   'Computer Vision',
-  'Artificial Intelligence'
+  'Survey'
 ];
 
 // 决定tag内文章排序，如果缺省的文章，倒排在这些之后
 const paperOrderInTag = {
-  'Human-AI Interaction': [33, 39, 1, 23],
-  'Intelligent Transportation': [38, 26, 16, 35, 32, 36, 37],
-  'Data Mining': [40, 18, 41, 45, 21],
-  'Natural Language Processing': [34, 33, 39, 1, 23, 30],
-  'Search and Recommendation': [27, 25, 17, 14, 24, 43, 44, 19, 15, 6, 4, 5, 3, 2, 8, 9],
-  'Computer Vision': [7, 29, 32],
-  'Artificial Intelligence': [7]
+  'Intelligent Transportation': [38, 26, 16, 49, 32, 36, 35],
+  'Natural Language Processing': [34, 33, 39],
+  // 'Survey': [31, 9]
 }
 
 
